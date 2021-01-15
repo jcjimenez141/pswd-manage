256ec454c952f43d76d1fb4f1c7c6b18ee679352
jc.jimenez <jc.jimenez@fujitsu.com>
2021-01-15 16:29:40 +0800

    Commit Initial Version

---
+++ src/main/java/com/personal/pswmanage/PswManageApplication.java
@@ -0,0 +1,13 @@
+package com.personal.pswmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PswManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PswManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/pswmanage/PswManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.pswmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PswManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
